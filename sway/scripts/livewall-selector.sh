74a2d22424d42afb988bfa4ef349009eb2568fc7
OkaPmna <okapramn092@gmail.com>
2026-03-07 23:05:09 +0800

    add wallpaper for mpvpaper

---
+++ sway/scripts/livewall-selector.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# DIR Configuration
+VID_DIR="$HOME/Videos/livewall"
+DISABLE_STR="! DISABLE LIVE WALLPAPER"
+
+if [ ! -d "$VID_DIR" ]; then
+    notify-send "Error" "Directory $VID_DIR not found."
+    exit 1
+fi
+
+LIST=$(printf "%s\n%s" "$DISABLE_STR" "$(ls "$VID_DIR" | grep -E "\.(mp4|mkv|mov|webm)$")")
+
+SELECTION=$(echo "$LIST" | wofi --dmenu --prompt "Select Live Wallpaper..." --width 400 --height 300 --cache-file /dev/null)
+
+if [ -z "$SELECTION" ]; then
+    exit 0
+fi
+
+# Logic to turn off
+if [ "$SELECTION" = "$DISABLE_STR" ]; then
+    pkill mpvpaper
+    swaymsg reload
+    notify-send "Live Wallpaper" "Disabled"
+    exit 0
+fi
+
+# video selection
+pkill mpvpaper
+mpvpaper -o "--hwdec=vaapi --vo=gpu --no-audio --vf=scale=-2:720,fps=15 --loop" "*" "$VID_DIR/$SELECTION" &
+
+sleep 0.5
+swaymsg reload
+
+notify-send "Wallpaper Updated" "Now playing: $SELECTION"
+
+
