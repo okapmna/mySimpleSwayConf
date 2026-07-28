ff7638e242e493972f7d0aaf5bdc80734156c051
OkaPmna <okapramn092@gmail.com>
2026-07-28 10:05:20 +0800

    clean sway scripts comments

--- sway/scripts/brightness.sh
+++ sway/scripts/brightness.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-# Argumen: up | down
 ACTION=$1
-
-# Langkah perubahan (misal: 5%)
 STEP=5
 
-# Ubah kecerahan
 if [ "$ACTION" == "up" ]; then
     brightnessctl set +${STEP}%
 elif [ "$ACTION" == "down" ]; then
     brightnessctl set ${STEP}%-
 fi
 
-# Dapatkan nilai kecerahan saat ini dalam persen
 BRIGHTNESS=$(brightnessctl -m | awk -F, '{print $4}' | tr -d '%')
-
-# Kirim notifikasi dengan progress bar
 dunstify "Brightness" -h int:value:"$BRIGHTNESS" -r 2593 -i display-brightness-high-symbolic
